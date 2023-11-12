7ccee8cb187f9a7303b740757894df6b4e1bee69
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2023-11-13 00:44:23 +0600

    alberta form 2 created

--- src/components/Incorporation/StandardCorporation/AlbertaCorporation/Form2.tsx
+++ src/components/Incorporation/StandardCorporation/AlbertaCorporation/Form2.tsx
@@ -1,6 +1,6 @@
 export default function Form2() {
     return (
-        <form className="bdr py-10 space-y-10">
+        <form className=" py-10 space-y-10">
             <h1 className="text-2xl font-bold text-center">
                 {"Corporation's Address"}
             </h1>
@@ -8,47 +8,67 @@ export default function Form2() {
                 <h3 className="font-bold">
                     {"Enter the corporation's address"}
                 </h3>
-                <label htmlFor="street">
-                    <span>Street Address :</span>
-                    <input
-                        id="street"
-                        className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
-                        placeholder="Write here..."
-                    />
-                </label>
-                <label htmlFor="suite-unit">
-                    <span>Suite / Unit :</span>
-                    <input
-                        id="suite-unit"
-                        className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
-                        placeholder="Write here..."
-                    />
-                </label>
-                <label htmlFor="city">
-                    <span>City :</span>
-                    <input
-                        id="city"
-                        className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
-                        placeholder="Write here..."
-                    />
-                </label>
-                <label htmlFor="province">
-                    <span>Province :</span>
-                    <input
-                        id="province"
-                        className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
-                        placeholder="Write here..."
-                        value="Alberta"
-                    />
-                </label>
-                <label htmlFor="city">
-                    <span>City :</span>
-                    <input
-                        id="city"
-                        className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
-                        placeholder="Write here..."
-                    />
-                </label>
+                <section className="grid grid-cols-2 gap-2">
+                    <label
+                        htmlFor="street"
+                        className="inline-flex items-center gap-3 "
+                    >
+                        <span className="w-40 text-right">
+                            Street Address :
+                        </span>
+                        <input
+                            id="street"
+                            className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
+                            placeholder="Write here..."
+                        />
+                    </label>
+                    <label
+                        htmlFor="suite-unit"
+                        className="inline-flex items-center gap-3 "
+                    >
+                        <span className="w-40 text-right">Suite / Unit :</span>
+                        <input
+                            id="suite-unit"
+                            className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
+                            placeholder="Write here..."
+                        />
+                    </label>
+                    <label
+                        htmlFor="city"
+                        className="inline-flex items-center gap-3 "
+                    >
+                        <span className="w-40 text-right">City :</span>
+                        <input
+                            id="city"
+                            className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
+                            placeholder="Write here..."
+                        />
+                    </label>
+                    <label
+                        htmlFor="province"
+                        className="inline-flex items-center gap-3 "
+                    >
+                        <span className="w-40 text-right">Province :</span>
+                        <input
+                            id="province"
+                            className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
+                            placeholder="Write here..."
+                            value="Alberta"
+                            readOnly
+                        />
+                    </label>
+                    <label
+                        htmlFor="postal-code"
+                        className="inline-flex items-center gap-3 "
+                    >
+                        <span className="w-40 text-right">Postal Code :</span>
+                        <input
+                            id="postal-code"
+                            className="input input-sm input-bordered w-full max-w-xs bg-slate-900/10 text-sm font-bold text-gray-500 min-w-0"
+                            placeholder="Write here..."
+                        />
+                    </label>
+                </section>
             </div>
         </form>
     );
